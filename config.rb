9e5628a484930a279868dc56a8fc59880e9e4bba
Simon Worthington <simon@register-dynamics.co.uk>
2023-09-22 15:13:02 +0100

    Highlight commodity codes in special colours.

--- config.rb
+++ config.rb
@@ -49,6 +49,19 @@ module RetargetLinks
 end
 GovukTechDocs::TechDocsHTMLRenderer.prepend(RetargetLinks)
 
+module CommodityCodes
+  def codespan code
+    code = code.gsub(/\W+/, '')
+    if code =~ /^[0-9]{10}$/
+      parts = [code[0..3], code[4..5], code[6..7], code[8..9]]
+      parts.each_with_index.map {|code, index| "<span class=\"cc#{index}\">#{code}</span>" }.join('')
+    else
+      "<code>#{code}</code>"
+    end
+  end
+end
+GovukTechDocs::TechDocsHTMLRenderer.prepend(CommodityCodes)
+
 PREFIX = (ENV['GITHUB_REPOSITORY'] || '').partition('/')[-2..-1].join
 
 configure :build do
